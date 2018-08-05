50c362ec82b1d49b74dad59f4aeb5ced9ac64a93
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-05 00:27:21 +0000

    automatically backed up by learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -3,8 +3,10 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+    end
   end
-  
+
   def down
+    drop_table :users
   end
 end
