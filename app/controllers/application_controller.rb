18b4b2a7f4dc094719a14ef135dcf171c788428e
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-05 00:41:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,10 +33,9 @@ class ApplicationController < Sinatra::Base
 		user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-
-        redirect "/success"
+      redirect "/success"
     else
-        redirect "/failure"
+      redirect "/failure"
     end
 	end
 
